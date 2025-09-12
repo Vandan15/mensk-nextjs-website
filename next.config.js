5d8afe9c90859403e7659508f20434b7ee10bafe
vandan-khamkar-us <vandan.khamkar@logicwind.com>
2025-09-12 12:44:51 +0530

    add sample env & robots.txt

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
