8b4492a088ad9440f5b166ad8635f47650533c8d
vandan-khamkar-us <vandan.khamkar@logicwind.com>
2025-09-21 10:41:59 +0530

    update meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,8 +29,8 @@ export const metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      // { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      // { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180" },
@@ -38,6 +38,28 @@ export const metadata = {
     shortcut: ["/favicon.ico"],
   },
   manifest: "/manifest.json",
+  openGraph: {
+    title: "No.1 law & Commerce Consultancy Firm in UAE | Mensk Consultancy",
+    description: "Elevate your business strategy with MENSK Consultancy's specialized services in Marketing, HR, Management, and AI consultancy.",
+    url: "https://menskconsultancy.com",
+    siteName: "Mensk Consultancy",
+    images: [
+      {
+        url: "/assets/images/og/og-mensk.webp",
+        width: 1200,
+        height: 630,
+        alt: "Mensk Consultancy - UAE Business Consultancy Firm",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "No.1 law & Commerce Consultancy Firm in UAE | Mensk Consultancy",
+    description: "Elevate your business strategy with MENSK Consultancy's specialized services in Marketing, HR, Management, and AI consultancy.",
+    images: ["/assets/images/og/og-mensk.webp"],
+  },
   viewport: {
     width: "device-width",
     initialScale: 1,
